85fdba6c8ca2bc15b8a8148e4d28ab22d62a0e12
wjpiantkoski <wjpiantkoski@gmail.com>
2025-10-08 20:35:33 -0300

    feat: initialize SceneManager in Home component and render canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { SceneManager } from "@/managers/scene";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    SceneManager.init(document.getElementById("canvas") as HTMLCanvasElement);
+
+    return () => {
+      SceneManager.cleanup();
+    };
+  }, []);
+
   return (
     <div className="flex flex-col h-screen p-4">
-      <h1 className="text-2xl font-bold text-center">Geometry manipulator</h1>
+      <canvas id="canvas" />
     </div>
   );
 }
